9a5e11e10cfce72205da0ff648d25a5ae1d79a17
sliver14 <silverchristopher12@gmail.com>
2026-04-17 05:41:58 +0100

    Update next.config.ts with explicit settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
